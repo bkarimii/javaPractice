9e513ec0a483c8d105c4c7c720a8038a2834b16c
karimi <behrouzkarimi1993@gmail.com>
2025-07-01 00:21:13 +0100

    goat move method implemented as an abstract method

--- project1/src/main/java/uk/co/cpsd/javaproject1/Goat.java
+++ project1/src/main/java/uk/co/cpsd/javaproject1/Goat.java
@@ -5,4 +5,13 @@ public class Goat extends Animal {
     public Goat(int x,int y){
         super(x,y,10);
     }
+
+    @Override
+    public void move(int worldSize){
+        int dx= (int) (Math.random()*3)-1;
+        int dy= (int) (Math.random()*3)-1;
+
+        x = Math.max(0, Math.min(worldSize - 1, x + dx));
+        y = Math.max(0, Math.min(worldSize - 1, y + dy));
+    }
 }
