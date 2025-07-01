9566ddbb53807c296cce068ce960d2cee90894b4
karimi <behrouzkarimi1993@gmail.com>
2025-07-01 21:06:15 +0100

    goat's color implemented

--- project1/src/main/java/uk/co/cpsd/javaproject1/Goat.java
+++ project1/src/main/java/uk/co/cpsd/javaproject1/Goat.java
@@ -1,5 +1,7 @@
 package uk.co.cpsd.javaproject1;
 
+import java.awt.Color;
+
 public class Goat extends Animal {
     
     public Goat(int x,int y){
@@ -13,5 +15,10 @@ public class Goat extends Animal {
 
         x = Math.max(0, Math.min(worldSize - 1, x + dx));
         y = Math.max(0, Math.min(worldSize - 1, y + dy));
+    }
+
+    @Override
+    public Color getColor(){
+        return Color.RED;
     }
 }
