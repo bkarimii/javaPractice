3b53479f6e20d9b309863475dda1982a6ed113f9
karimi <behrouzkarimi1993@gmail.com>
2025-07-09 00:25:12 +0100

    world methods refactored based GrassExpiry class

--- project1/src/main/java/uk/co/cpsd/javaproject1/World.java
+++ project1/src/main/java/uk/co/cpsd/javaproject1/World.java
@@ -1,93 +1,73 @@
 package uk.co.cpsd.javaproject1;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class World {
     public final int size = 10;
-    private int[][] grassAge = new int[size][size];
-    private int[][] grassBornTick= new int[size][size];
     private List<Animal> animals;
     private int totalTicks;
     private final int MAX_GRASS_AGE=25;
+    List<GrassExpiry> grassDeathTimeList=new ArrayList<>();
 
     public World(int numOfGoats){
         animals=new ArrayList<>();
         for(int i=0;i<numOfGoats;i++){
             animals.add(new Goat((int)(Math.random() * size),(int)(Math.random() * size)));
         }
-
-        for(int i=0;i<size;i++){
-            for(int j=0;j<size;j++){
-                grassAge[i][j]=0;
-            }
-        }
     }
 
 
-    public void growGrass() {
-        int x = (int)(Math.random() * size);
-        int y = (int)(Math.random() * size);
+    // public void growGrass() {
+    //     int x = (int)(Math.random() * size);
+    //     int y = (int)(Math.random() * size);
         
-        if(grassAge[x][y]==0){
-            grassAge[x][y]=1;
-        }
-        System.out.println("Grass grew at (" + x + "," + y + ") at total tick: " + totalTicks);
-    }
+    //     if(grassAge[x][y]==0){
+    //         grassAge[x][y]=1;
+    //     }
+    //     System.out.println("Grass grew at (" + x + "," + y + ") at total tick: " + totalTicks);
+    // }
 
     // new implementation
     public void growGrassTwo() {
         int x = (int)(Math.random() * size);
         int y = (int)(Math.random() * size);
-        
-        if(grassBornTick[x][y]==0){
-            grassBornTick[x][y]=totalTicks;
-        }
+        int deathTick=totalTicks+MAX_GRASS_AGE;
+        // if(hasGrass(x, y)) return;
+        GrassExpiry newGrass=new GrassExpiry(x, y, deathTick);
+        grassDeathTimeList.add(newGrass);
         System.out.println("Grass grew at (" + x + "," + y + ") at total tick: " + totalTicks);
     }
 
-    public boolean hasGrass(int x, int y) {
-        return grassAge[x][y]>0;
+    public boolean hasGrass(int x, int y){
+        return grassDeathTimeList.stream().anyMatch(g-> g.x==x && g.y==y);
     }
 
-    public void removeGrass(int x, int y) {
-        grassAge[x][y] = 0;
+    public void removeDeadGrass(){
+        Iterator<GrassExpiry> iterateGrass= grassDeathTimeList.iterator();
+
+        while(iterateGrass.hasNext()){
+            GrassExpiry grass=iterateGrass.next();
+            if(grass.deathTick==totalTicks){
+                iterateGrass.remove();
+            }
+        }
+
+    }
+    public void removeGrass(int x, int y){
+        grassDeathTimeList.removeIf(g->g.x==x&&g.y==y);
     }
 
     public Stream<Animal> animals(){
         return animals.stream();
     }
 
-    public void ageIncreaser(int worldSize){
-        for(int i=0;i<size;i++){
-            for(int j=0;j<size;j++){
-                if(grassAge[i][j]>0){
-                    grassAge[i][j]++;
-                }
-            }
-        }
-    }
 
-    public void grassDies(int worldSize,int maxAge){
-        for(int i=0;i<size;i++){
-            for(int j=0; j<size; j++){
-                totalTicks++;
-                if(grassAge[i][j]>maxAge){
-                    removeGrass(i, j);
-                    System.out.println("Grass at [" + i + " , " + j + "] died of old age after " + maxAge + " ticks (at total tick: " + totalTicks + ").");
-                }
-            }
-        }
-    }
     public void tick() {
 
-        ageIncreaser(size);
-
-        growGrass();
-
-        final int MAX_GRASS_AGE=20;
-        grassDies(size, MAX_GRASS_AGE);
+        growGrassTwo();
     }
 
     public void moveAnimals(){
