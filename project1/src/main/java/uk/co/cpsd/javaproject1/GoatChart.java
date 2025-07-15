b645a561596909b9ecf76bae131be5632a8551eb
karimi <behrouzkarimi1993@gmail.com>
2025-07-15 23:58:41 +0100

    chart to show number of goats and grass

--- project1/src/main/java/uk/co/cpsd/javaproject1/GoatChart.java
+++ project1/src/main/java/uk/co/cpsd/javaproject1/GoatChart.java
@@ -2,16 +2,22 @@ package uk.co.cpsd.javaproject1;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartUtils;
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 import javax.swing.*;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class GoatChart extends JFrame {
 
     public GoatChart(List<Integer> goatCounts, List<Integer> grassCounts) {
-        setTitle("Population Over Time");
+
+        try {
+            setTitle("Population Over Time");
         setSize(700, 450);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -30,7 +36,12 @@ public class GoatChart extends JFrame {
                 dataset
         );
 
+        ChartUtils.saveChartAsPNG(new File("chart.png"), chart, 600, 500);
         ChartPanel chartPanel = new ChartPanel(chart);
+
         setContentPane(chartPanel);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 }
