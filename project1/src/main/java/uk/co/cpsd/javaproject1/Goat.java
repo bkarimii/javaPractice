7e0d85ac0f283d642f0b6be29785c3e3678d9f6e
karimi <behrouzkarimi1993@gmail.com>
2025-07-16 23:05:18 +0100

    if goats are not hungry they don't eat, 90%

--- project1/src/main/java/uk/co/cpsd/javaproject1/Goat.java
+++ project1/src/main/java/uk/co/cpsd/javaproject1/Goat.java
@@ -2,9 +2,13 @@ package uk.co.cpsd.javaproject1;
 
 import java.awt.Color;
 
+
 public class Goat extends Animal {
     
     public final int ENERGY_DECREASE_INTERVAL=5;
+    public final int HUNGER_TRESHHOLDS=80;
+    public final double CHANCE_TO_EAT_IF_FULL=0.2;
+    public final int MAX_GOAT_ENERGY_LEVEL=130;
 
     private Gender gender;
 
@@ -35,6 +39,8 @@ public class Goat extends Animal {
         y = Math.max(0, Math.min(worldSize - 1, y + dy));
     }
 
+    
+
     @Override
     public Color getColor(){
         return Color.RED;
@@ -42,11 +48,15 @@ public class Goat extends Animal {
 
     @Override
     public void act(World world){
-        move(world.size);
-        if(world.hasGrass(x, y)){
+
+        boolean shallIEat=Math.random()<CHANCE_TO_EAT_IF_FULL;
+        boolean isGrass= world.hasGrass(x, y);
+        if(isGrass&&(isHungry() && shallIEat)){
             eatGrass();
             world.removeGrass(x, y);
+            
         }
+        move(world.size);
 
     }
 
@@ -64,25 +74,35 @@ public class Goat extends Animal {
     }
 
     @Override
-public Animal tryReproduceWith(Animal partner) {
-    if (!(partner instanceof Goat)) return null;
-    Goat other = (Goat) partner;
+    public Animal tryReproduceWith(Animal partner) {
+        if (!(partner instanceof Goat)) return null;
+        Goat other = (Goat) partner;
 
-    boolean samePosition = this.x == other.x && this.y == other.y;
-    boolean differentGender = this.gender != other.gender;
-    boolean bothEnergetic = this.energyLevel >= 15 && other.energyLevel >= 15;
-    boolean notSameAnimal = this.animalId != other.animalId;
+        boolean samePosition = this.x == other.x && this.y == other.y;
+        boolean differentGender = this.gender != other.gender;
+        boolean bothEnergetic = this.energyLevel >= 20 && other.energyLevel >= 20;
+        boolean notSameAnimal = this.animalId != other.animalId;
 
-    if (samePosition && differentGender && bothEnergetic && notSameAnimal) {
-        this.decreaseEnergyBy(2);
-        other.decreaseEnergyBy(2);
-        Goat baby = new Goat(this.x, this.y);
-        baby.energyLevel = 5;
-        System.out.println("🐐 Baby goat born at (" + x + ", " + y + ")");
-        return baby;
+        if (samePosition && differentGender && bothEnergetic && notSameAnimal) {
+            this.decreaseEnergyBy(2);
+            other.decreaseEnergyBy(2);
+            Goat baby = new Goat(this.x, this.y);
+            baby.energyLevel = 5;
+            System.out.println("🐐 Baby goat born at (" + x + ", " + y + ")");
+            return baby;
+        }
+
+        return null;
     }
 
-    return null;
-}
+    @Override
+    public boolean isHungry(){
+        return this.energyLevel<HUNGER_TRESHHOLDS;
+    }
+
+    @Override
+    public boolean isSick(){
+        return this.energyLevel>130;
+    }
 
 }
