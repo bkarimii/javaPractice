91d13247cc5b559925ac7b6b2553b5dcdfcb665f
karimi <behrouzkarimi1993@gmail.com>
2025-07-16 23:06:30 +0100

    simulate without userInterface for a quick simulation

---
+++ project1/src/main/java/uk/co/cpsd/javaproject1/SimulatorRunner.java
@@ -0,0 +1,18 @@
+package uk.co.cpsd.javaproject1;
+
+public class SimulatorRunner {
+    public static void noGUISimulation(int ticks,int numOfGoats){
+        World world=new World(numOfGoats);
+
+        for(int i=1;i<=ticks;i++){
+            world.moveAnimals();
+            world.tick();
+        }
+
+        world.writeToCSV(world.getGoatHistory(), world.getGrassHistory());
+        GoatChart chart=new GoatChart(world.getGoatHistory(), world.getGrassHistory());
+        chart.setVisible(true);
+    }
+
+
+}
