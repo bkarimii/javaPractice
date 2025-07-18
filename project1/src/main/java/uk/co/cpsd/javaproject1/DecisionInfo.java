dbeda0ba315e5cb5e334c7269bea13ef6af21463
karimi <behrouzkarimi1993@gmail.com>
2025-07-18 14:27:47 +0100

    this class handles the decisions make by each animal

---
+++ project1/src/main/java/uk/co/cpsd/javaproject1/DecisionInfo.java
@@ -0,0 +1,33 @@
+package uk.co.cpsd.javaproject1;
+
+import java.awt.Point;
+
+public class DecisionInfo {
+    
+    private DecisionType type;
+    private Point nextPos;
+
+    public enum DecisionType{
+        EAT,
+        FLEE,
+        REPRODUCE,
+        WANDER,
+    }
+
+    public DecisionInfo(DecisionType type, Point nextPos){
+        this.type=type;
+        this.nextPos=nextPos;
+    }
+
+    public DecisionType getType(){
+        return type;
+    }
+
+    public Point getNextPos(){
+        return nextPos;
+    }
+
+
+
+
+}
